726c222f9133877871645ecaca182c74c28ea20f
artrixdotdev <sefu786@outlook.com>
2025-12-05 12:04:16 -0800

    refactor: Move drizzle config to env package

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,19 +1,15 @@
 import type { Config } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-   throw new Error("Missing POSTGRES_URL");
-}
+import { dbEnv } from "@ota/env";
 
-if (!process.env.DATABASE_AUTH_TOKEN && process.env.NODE_ENV === "production") {
-   throw new Error("Missing DATABASE_AUTH_TOKEN");
-}
+const env = dbEnv();
 
 export default {
    schema: "./src/schema.ts",
    dialect: "turso",
    dbCredentials: {
-      url: process.env.DATABASE_URL,
-      authToken: process.env.DATABASE_AUTH_TOKEN,
+      url: env.DATABASE_URL,
+      authToken: env.DATABASE_AUTH_TOKEN,
    },
    casing: "snake_case",
 } satisfies Config;
