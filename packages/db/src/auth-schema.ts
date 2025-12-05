691beb02d46e5d15b7b164daf802223ce65d637d
artrixdotdev <sefu786@outlook.com>
2025-12-04 23:28:26 -0800

    refactor: Make shorthand for `timestamp` and `boolean` DB datatypes

--- packages/db/src/auth-schema.ts
+++ packages/db/src/auth-schema.ts
@@ -1,49 +1,53 @@
-import { sqliteTable } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const user = sqliteTable("user", (t) => ({
-   id: t.text().primaryKey(),
-   name: t.text().notNull(),
-   createdAt: t.integer({ mode: "timestamp" }).notNull(),
+// sqlite is a bit dumb and doesn't support boolean or timestamp columns natively
+
+// const boolean = () => integer({ mode: "boolean" });
+const timestamp = () => integer({ mode: "timestamp" });
+
+export const user = sqliteTable("user", () => ({
+   id: text().primaryKey(),
+   name: text().notNull(),
+   createdAt: timestamp().notNull(),
 }));
 
-export const session = sqliteTable("session", (t) => ({
-   id: t.text().primaryKey(),
-   expiresAt: t.integer({ mode: "timestamp" }).notNull(),
-   token: t.text().notNull().unique(),
-   createdAt: t.integer({ mode: "timestamp" }).notNull(),
-   updatedAt: t.integer({ mode: "timestamp" }).notNull(),
-   ipAddress: t.text(),
-   userAgent: t.text(),
-   userId: t
-      .text()
+export const session = sqliteTable("session", () => ({
+   id: text().primaryKey(),
+   expiresAt: timestamp().notNull(),
+   token: text().notNull().unique(),
+   createdAt: timestamp().notNull(),
+   updatedAt: timestamp().notNull(),
+   ipAddress: text(),
+   userAgent: text(),
+   userId: text()
       .notNull()
       .references(() => user.id, { onDelete: "cascade" }),
 }));
 
 export const account = sqliteTable("account", (t) => ({
-   id: t.text().primaryKey(),
-   accountId: t.text().notNull(),
-   providerId: t.text().notNull(),
+   id: text().primaryKey(),
+   accountId: text().notNull(),
+   providerId: text().notNull(),
    userId: t
       .text()
       .notNull()
       .references(() => user.id, { onDelete: "cascade" }),
-   accessToken: t.text(),
-   refreshToken: t.text(),
-   idToken: t.text(),
-   accessTokenExpiresAt: t.integer({ mode: "timestamp" }),
-   refreshTokenExpiresAt: t.integer({ mode: "timestamp" }),
-   scope: t.text(),
-   password: t.text(),
-   createdAt: t.integer({ mode: "timestamp" }).notNull(),
-   updatedAt: t.integer({ mode: "timestamp" }).notNull(),
+   accessToken: text(),
+   refreshToken: text(),
+   idToken: text(),
+   accessTokenExpiresAt: timestamp(),
+   refreshTokenExpiresAt: timestamp(),
+   scope: text(),
+   password: text(),
+   createdAt: timestamp().notNull(),
+   updatedAt: timestamp().notNull(),
 }));
 
 export const verification = sqliteTable("verification", (t) => ({
-   id: t.text().primaryKey(),
-   identifier: t.text().notNull(),
-   value: t.text().notNull(),
-   expiresAt: t.integer({ mode: "timestamp" }).notNull(),
-   createdAt: t.integer({ mode: "timestamp" }),
-   updatedAt: t.integer({ mode: "timestamp" }),
+   id: text().primaryKey(),
+   identifier: text().notNull(),
+   value: text().notNull(),
+   expiresAt: timestamp().notNull(),
+   createdAt: timestamp(),
+   updatedAt: timestamp(),
 }));
