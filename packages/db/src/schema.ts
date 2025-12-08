c9f6e66cbe81a81c36811da05556c03dfde64bee
artrixdotdev <sefu786@outlook.com>
2025-12-08 14:00:24 -0800

    refactor: Reorganize db file structure

--- packages/db/src/schema.ts
+++
@@ -1,6 +0,0 @@
-// import { sql } from "drizzle-orm";
-// import { pgTable } from "drizzle-orm/pg-core";
-// import { createInsertSchema } from "drizzle-zod";
-// import { z } from "zod/v4";
-
-export * from "./auth-schema";
