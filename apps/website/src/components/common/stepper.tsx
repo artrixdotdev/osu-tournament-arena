9b37d82b8278cbb37a2ec00dde90e36436ded97e
artrixdotdev <sefu786@outlook.com>
2025-12-12 16:17:10 -0800

    feat: Allow for custom styling on Stepper component

--- apps/website/src/components/common/stepper.tsx
+++ apps/website/src/components/common/stepper.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 
+import { cn } from "@ota/ui";
+
 export const useStepper = (stepCount: number) => {
    const router = useRouter();
    const searchParams = useSearchParams();
@@ -23,17 +25,23 @@ export const useStepper = (stepCount: number) => {
    return { currentStep, setStep, bumpStep, resetStep };
 };
 
-export const Stepper: React.FC<{ children: React.ReactNode[] }> = ({
-   children,
-}) => {
+export interface StepperProps {
+   children: React.ReactNode[];
+   className?: string;
+}
+
+export const Stepper: React.FC<StepperProps> = ({ children, className }) => {
    const stepper = useStepper(children.length);
    return (
-      <div className="flex flex-col gap-4">
-         <div>{children[stepper.currentStep]}</div>
+      <div className={cn("flex flex-col gap-4", className)}>
+         {children[stepper.currentStep]}
          <div className="flex justify-center gap-2 p-4">
             {children.map((_, index) => (
                <div
-                  className="bg-accent h-2 w-full cursor-pointer rounded"
+                  className={cn(
+                     `bg-secondary h-2 w-full cursor-pointer rounded transition-colors duration-300`,
+                     index === stepper.currentStep ? "bg-accent" : "",
+                  )}
                   key={index}
                   onClick={() => stepper.setStep(index)}
                />
