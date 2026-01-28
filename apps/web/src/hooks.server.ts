41a44ec679e35862273c1b0ebe0af121abe25b37
artrixdotdev <sefu786@outlook.com>
2026-01-28 09:47:36 -0800

    fix: Add types to `hooks.server.ts`

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -2,6 +2,7 @@ import { building } from "$app/environment";
 import { auth } from "$lib/auth";
 import { svelteKitHandler } from "better-auth/svelte-kit";
 
-export async function handle({ event, resolve }) {
-   return svelteKitHandler({ event, resolve, auth, building });
-}
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({ event, resolve }) =>
+   svelteKitHandler({ event, resolve, auth, building });
