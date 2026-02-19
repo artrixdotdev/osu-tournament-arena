dbb14f0f5b08e1b053a3829aba519ce5975ad704
artrixdotdev <sefu786@outlook.com>
2026-02-19 09:57:47 -0800

    refactor: cleanup file structure for bars

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
    import favicon from "$lib/assets/favicon.svg";
-   import AppSidebar from "$lib/components/app-sidebar.svelte";
-   import SidebarHeader from "$lib/components/sidebar-header.svelte";
+   import AppSidebar from "$lib/components/sidebar.svelte";
+   import Topbar from "$lib/components/topbar.svelte";
    import { ModeWatcher } from "mode-watcher";
 
-   import { Separator } from "@ota/ui/components/separator/index.ts";
    import * as Sidebar from "@ota/ui/components/sidebar/index.ts";
 
    import "@ota/tailwind-config/theme";
@@ -21,24 +20,7 @@
    <ModeWatcher />
 
    <Sidebar.Provider class="bg-sidebar flex flex-col">
-      <header
-         class="bg-sidebar relative z-20 flex w-full items-center py-2 pr-4 pl-2"
-      >
-         <div class="flex items-center gap-2">
-            <SidebarHeader />
-            <Separator orientation="vertical" class="mr-2 h-4" />
-            <a class="flex items-center gap-2" href="/">
-               <div
-                  class="bg-primary flex size-7 items-center justify-center rounded-md text-xs font-bold text-white"
-               >
-                  O
-               </div>
-               <span class="text-sm font-semibold tracking-wide">
-                  osu! tournament arena
-               </span>
-            </a>
-         </div>
-      </header>
+      <Topbar />
       <div class="flex flex-1 pr-2 pb-2">
          <AppSidebar user={data.user} />
          <main class="bg-background flex-1 rounded-xl">
