33d2ae1d6357c194d89e2934b23cca7b9e30a0bd
artrixdotdev <sefu786@outlook.com>
2026-02-25 09:31:19 -0800

    build: fix typescript & eslint errors causing odd behavior

--- apps/web/src/lib/flags.ts
+++ apps/web/src/lib/flags.ts
@@ -1,5 +1,5 @@
 import type { Locale } from "$paraglide/runtime";
-import { type Component } from "svelte";
+import type { Component } from "svelte";
 import * as Flags from "svelte-flag-icons";
 
 export const localeFlags: Record<Locale, Component> = {
